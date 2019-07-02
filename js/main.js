5b9d8092104216396c3eff2229a3c7c4d7b6a1fa
xuyuefeng@192.168.0.119:29418 <2670476383@qq.com>
2019-07-02 22:34:44 +0800

    翻牌记录页面

--- js/main.js
+++ js/main.js
@@ -11,7 +11,6 @@ var mainCardWrapper = document.querySelector(".main-card"),
 var qString = JSON.parse(decodeURIComponent(getQueryString())),
     cardBagList = qString.cardBagList,
     randList = qString.randList;
-console.log(qString);
 // 渲染卡包数量
 function renderBag() {
     for (i = 0, len = cardBags.length; i < len; i++) {
@@ -31,32 +30,19 @@ function updateTips(newTips) {
 }
 // 为开启按钮绑定点击事件
 function bindOpenButtonEvent() {
-    var clickSameCount = 0;
     for (i = 0, len = openButtons.length; i < len; i++) {
         (function(i){
             openButtons[i].addEventListener('click', function (e) {
                 var nums = this.getAttribute('data-nums'),
                     cards = this.getAttribute('data-cards'),
-                    index = this.getAttribute('data-index'),
                     emptyHtmlString = "<div class=\"main-card__empty\">暂无卡牌~</div>",
                     htmlString = "";
                 if (hasFlip()) {
                     return;
                 }
-                if (parseInt(index) === i) {
-                    console.log(clickSameCount);
-                    if (clickSameCount > 0) {
-                        return;
-                    }
-                    clickSameCount++;
-                } else {
-                    clickSameCount = 0;
-                }
                 if (parseInt(nums) > 0) {
-                    var randGroup = [];
                     for (i = 0; i < cards; i++) {
                         var randBonus = randList[Math.floor(Math.random() * maxCardsLen)];
-                        randGroup.push(randBonus);
                         htmlString += "<div class=\"main-card__item\" data-type=\"card\">\n" +
                             "\t\t\t\t<div class=\"front\">\n" +
                             "\t\t\t\t\t<img class=\"card-bg\" src=\"" + cardImagePath + "\" alt=\"\">\n" +
@@ -66,8 +52,6 @@ function bindOpenButtonEvent() {
                             "\t\t\t\t</div>\n" +
                             "\t\t\t</div>";
                     }
-                    randResult = randResult.concat(randGroup);
-                    console.log(randResult);
                     for (i = 0; i < maxCardsLen - cards; i++) {
                         htmlString += "<div class=\"main-card__item--empty\"></div>";
                     }
@@ -91,10 +75,12 @@ function bindCardEvent() {
        // 点击了卡牌
        if (thisClick.getAttribute('data-type') === 'card') {
            thisClick.setAttribute('class', 'main-card__item main-card__item--flip');
+           pushFlipText(thisClick);
            if (checkCardFlipAll()) {
                 setTimeout(function () {
                     updateCardBagList();
                     if (checkBagAll()) {
+                        cookie.set('randResult', JSON.stringify(randResult));
                         updateTips('翻牌结束，去看看记录吧~');
                     }
                     mainCardWrapper.innerHTML = "<div class=\"main-card__success\">翻牌完成~</div>";
@@ -102,6 +88,18 @@ function bindCardEvent() {
            }
        }
     });
+}
+function pushFlipText(clickDom) {
+    var clickChild = clickDom.querySelector(".end p"),
+        clickText = clickChild.innerText;
+    if (clickChild.getAttribute('class') !== 'selected') {
+        var obj = {},
+            now = formatTime(new Date());
+        obj.bonus = clickText;
+        obj.time = now;
+        randResult.push(obj);
+    }
+    clickChild.setAttribute('class', 'selected');
 }
 // 检测一组卡牌是否全翻
 function  checkCardFlipAll() {
@@ -139,7 +137,15 @@ function updateCardBagList() {
 function triggerFirstOpenButton() {
     openButtons[0].click();
 }
+function onRecord() {
+    return false;
+}
 window.onload = function () {
+    if (cookie.get('randResult')) {
+        var body = document.querySelector("body");
+        body.innerHTML = "<h2 class='expire-page'>页面已过期</h2>";
+        return;
+    }
     updateTips();
     renderBag();
     bindOpenButtonEvent();
--- js/util.js
+++ js/util.js
@@ -26,7 +26,35 @@ function _getQueryString(queryList) {
 function getQueryString() {
     return _getQueryString(getQuerys());
 }
+function formatTime(d) {
+    var year = d.getFullYear(),
+        month = formatNumber(d.getMonth() + 1),
+        date = formatNumber(d.getDate()),
+        hour = formatNumber(d.getHours()),
+        minute = formatNumber(d.getMinutes());
+    return [year, month, date].join("-") + "\0" + [hour, minute].join(":");
+}
+function formatNumber(n) {
+    n = n / 10 >= 1 ? n : "0" + n;
+    return n;
+}
 function Cookie() {}
 Cookie.prototype.set = function (key, value) {
-    document.cookie = key + "=" + value;
+    var now = new Date();
+    now.setTime(now.getTime() + 1000 * 60 * 30);    // 半小时过期
+    document.cookie = key + "=" + value + "; expires=" + now.toUTCString();
+};
+Cookie.prototype.get = function (key) {
+    var cookie = document.cookie.replace(/\s/g, ""),
+        arr = cookie.split(";");
+    for (var i = 0, len = arr.length; i < len; i++) {
+        var item = arr[i],
+            _arr = item.split("="),
+            _key = _arr[0],
+            _value = _arr[1];
+        if (key === _key) {
+            return _value;
+        }
+    }
+    return false;
 };
