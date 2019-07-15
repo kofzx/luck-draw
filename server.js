aa4bafc40bf169300b01fde8c95a1c0564a07e46
xuyuefeng@192.168.0.119:29418 <2670476383@qq.com>
2019-07-15 20:52:21 +0800

    server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+app.get('/', function (req, res) {
+   res.sendFile(__dirname + '/views/index.html');
+});
+app.listen(1337, function () {
+    console.log('server is running at 127.0.0.1:1337');
+});
